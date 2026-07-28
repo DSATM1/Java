4dee9b2dc0f222bf4e6b1333cbe8d95f6d50cbcf
Suraj <surajsppokale@gmail.com>
2026-07-28 12:53:03 +0530

    add float variable and output to hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,7 @@
 public class hello{
     public static void main(String[] args){
+        float f = 3.21f;
+        System.out.println(f);
         int n = 3;
         System.out.println(n);
         System.out.println("Happy Evening");
