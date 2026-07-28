76974b2cf9c92a70d2bcd2c5d6b0c4d86d85a64e
Suraj <surajsppokale@gmail.com>
2026-07-28 12:55:42 +0530

    refactor output statements in hello.java for clarity

--- hello.java
+++ hello.java
@@ -1,9 +1,11 @@
 public class hello{
     public static void main(String[] args){
+        double d = 1.324;
+        System.out.println("Double = " + d);
         float f = 3.21f;
-        System.out.println(f);
+        System.out.println("Float = " + f);
         int n = 3;
-        System.out.println(n);
+        System.out.println("Number = " + n);
         System.out.println("Happy Evening");
         System.out.println("Happy Sunday Suraj");
     }
