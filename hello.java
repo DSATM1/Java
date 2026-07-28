dfe52ca0e81942bfc49763691473dcff7d274c94
Suraj <surajsppokale@gmail.com>
2026-07-28 11:53:52 +0530

    add variable initialization and output for integer in hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,7 @@
 public class hello{
     public static void main(String[] args){
+        int n = 3;
+        System.out.println(n);
         System.out.println("Happy Evening");
         System.out.println("Happy Sunday Suraj");
     }
