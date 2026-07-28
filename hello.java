4512634af55d20ea4c6cb35df158970c88f62143
Suraj <surajsppokale@gmail.com>
2026-07-28 13:04:08 +0530

    refactor output statements in hello.java for consistency and clarity

--- hello.java
+++ hello.java
@@ -5,9 +5,17 @@ public class hello{
         float f = 3.21f;
         System.out.println("Float = " + f);
         int n = 3;
-        System.out.println("Number = " + n);
-        System.out.println("Happy Evening");
-        System.out.println("Happy Sunday Suraj");
+        System.out.println("Int Number = " + n);
+        byte b = 123;
+        System.out.println("Byte = " + b);
+        char c = 'k';
+        System.out.println("Character = " + c);
+        short s = 432;
+        System.out.println("Short = " + s);
+        long ln = 32l;
+        System.out.println("Long = " + ln);
+        boolean bol = true;
+        System.out.println("Boolean = " + bol);
     }
 }
 
